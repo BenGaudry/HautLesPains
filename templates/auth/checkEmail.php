c6baaaf3e14ef4360fa3603c0e852600b6fbf9c3
BenGaudry <totoro42360@gmail.com>
2022-09-03 17:45:49 +0200

    OTP verif front

--- templates/auth/checkEmail.php
+++ templates/auth/checkEmail.php
@@ -3,10 +3,21 @@
 require_once '../../res/components/header.php';
 session_start();
 
-print(mail($_SESSION['email'], "Nouvelle connexion", "Nouvelle connexion détectée"));
+// mail($_SESSION['email'], "Nouvelle connexion", "Nouvelle connexion détectée");
 
 ?>
 
-<h1 class="page-title">Checking email</h1>
+<h1 class="page-title">Vérifier l'email</h1>
+<p class="text-centered">Un code OTP a 6 chiffres vous a été envoyé par email</p>
+<p class="text-centered">Vous pouvez le taper, ou le coller çi-dessous</p>
+
+<div class="otp-field">
+  <input type="text" name="" id="" maxlength="1">
+  <input type="text" name="" id="" maxlength="1">
+  <input class="space" type="text" name="" id="" maxlength="1">
+  <input type="text" name="" id="" maxlength="1">
+  <input type="text" name="" id="" maxlength="1">
+  <input type="text" name="" id="" maxlength="1">
+</div>
 
 <?php require_once '../../res/components/footer.php'; ?>
