995cc5f95d163653c087d7ccac23b9313ccfda4f
BenGaudry <totoro42360@gmail.com>
2022-09-09 19:38:54 +0200

    Deleting account upgrades

--- res/scripts/main.js
+++ res/scripts/main.js
@@ -1,91 +1,139 @@
 // Start scripting here
 
 function toggleMenu() {
-  let btn = document.getElementById('menu-btn');
-  btn.classList.toggle('menu-btn-active');
+  let btn = document.getElementById("menu-btn");
+  btn.classList.toggle("menu-btn-active");
 }
 
-window.onscroll = function() { //Fonction appelée quand on descend la page
-  let btn = document.getElementById('scrollUp')
-  let x = document.body.scrollTop || document.documentElement.scrollTop
-  if (x > 80 ) {  // Quand on est à 200pixels du haut de page,
-    btn.style.opacity = '1'
-  } else { 
-    btn.style.opacity = '0'
+window.onscroll = function () {
+  //Fonction appelée quand on descend la page
+  let btn = document.getElementById("scrollUp");
+  let x = document.body.scrollTop || document.documentElement.scrollTop;
+  if (x > 80) {
+    // Quand on est à 200pixels du haut de page,
+    btn.style.opacity = "1";
+  } else {
+    btn.style.opacity = "0";
   }
-}
+};
 
 function addCountryCodes(selectId) {
   fetch("http://localhost/HautLesPains2/config/db/country-codes.json", {
-		"method": "GET",
-	})
-	.then(response => response.json())
-	.then(data => {
-    for (i = 1; i < 242; i++) {
-      var select = document.getElementById (selectId);
-      let text = data[i].name + ' - ' + data[i].dial_code
-      if(data[i].dial_code === '+33'){
-        var newOption = new Option (text, data[i].dial_code, true)
-      } else {
-        var newOption = new Option (text, data[i].dial_code)
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      for (i = 1; i < 242; i++) {
+        var select = document.getElementById(selectId);
+        let text = data[i].name + " - " + data[i].dial_code;
+        if (data[i].dial_code === "+33") {
+          var newOption = new Option(text, data[i].dial_code, true);
+        } else {
+          var newOption = new Option(text, data[i].dial_code);
+        }
+        select.options.add(newOption);
       }
-      select.options.add (newOption);
-    }
-	})
-	.catch(err => {
-		console.error(err)
-	});
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
 
-function closeNotification(){
+function closeNotification() {
   document.getElementById("notification").style.opacity = "0";
   setTimeout(() => {
-      document.getElementById("notification").style.display = "none"
+    document.getElementById("notification").style.display = "none";
   }, 300);
 }
 
 // OTP verif
-const inputs = document.querySelectorAll(".otp-field input")
+const inputs = document.querySelectorAll(".otp-field input");
 inputs.forEach((input, index) => {
-  input.dataset.index = index
-  input.addEventListener("paste", handleOnPasteOtp)
-  input.addEventListener("keyup", handleOtp)
-})
+  input.dataset.index = index;
+  input.addEventListener("paste", handleOnPasteOtp);
+  input.addEventListener("keyup", handleOtp);
+});
 
 function handleOnPasteOtp(e) {
-  const data = e.clipboardData.getData("text")
-  const value = data.split("")
-  if(value.length === inputs.length) {
+  const data = e.clipboardData.getData("text");
+  const value = data.split("");
+  if (value.length === inputs.length) {
     inputs.forEach((input, index) => (input.value = value[index]));
-    submit()
+    submit();
   }
 }
 
 function handleOtp(e) {
-  const input = e.target
-  let value = input.value
-  input.value = ""
-  input.value = value ? value[0] : ""
+  const input = e.target;
+  let value = input.value;
+  input.value = "";
+  input.value = value ? value[0] : "";
 
-  let fieldIndex = input.dataset.index
-  if(value.length > 0 && fieldIndex < inputs.length) {
-    input.nextElementSibling.focus()
+  let fieldIndex = input.dataset.index;
+  if (value.length > 0 && fieldIndex < inputs.length) {
+    input.nextElementSibling.focus();
   }
 
-  if(e.key == "Backspace" && fieldIndex > 0) {
-    input.previousElementSibling.focus()
+  if (e.key == "Backspace" && fieldIndex > 0) {
+    input.previousElementSibling.focus();
   }
 
-  if(fieldIndex === inputs.length) {
-    submit()
+  if (fieldIndex === inputs.length) {
+    submit();
   }
 }
 
 function submit() {
-  let otp = ""
+  let otp = "";
   inputs.forEach((input) => {
-    otp += input.value
-  })
+    otp += input.value;
+  });
 
-  console.log(otp)
+  console.log(otp);
 }
+
+// Confirmation de la suppression du compte
+// variables
+const confirmWindow = document.getElementById("del-acc-confirm-window");
+const confirmEmailInput = document.getElementById("delete-acc-email-confirm");
+const confirmDeleteBtn = document.getElementById('delete-acc-btn-confirm')
+
+// Afficher la fenetre de confirmation
+document.getElementById("delete-acc-btn").addEventListener("click", () => {
+  // on affiche la popup
+  confirmWindow.classList.add("active-confirm-window")
+});
+
+// Checker si l'email correspond
+confirmEmailInput.addEventListener("input", (e) => {
+  console.log(e);
+
+  if (e.inputType !== "insertText") {
+    // L'utilisateur n'a pas tapé son email
+    alert("Le collage ou les suggestions d'email ne sont pas autorisés !");
+    e.preventDefault();
+    confirmEmailInput.value = "";
+    confirmDeleteBtn.disabled = true
+
+  } else {
+
+    // L'utilisateur a tapé son email
+    const email = document.getElementById(
+      "delete-acc-email-to-confirm"
+    ).textContent;
+    let inputVal = confirmEmailInput.value;
+    if (inputVal === email) {
+      // la valeur correspond, on enable le btn
+      confirmDeleteBtn.disabled = false
+    } else {
+      // la valeur ne correspond pas, on disable le btn
+      confirmDeleteBtn.disabled = true
+    }
+  }
+});
+
+// L'utilisateur annule la suppresion
+document.getElementById('cancel-deleting-acc').addEventListener('click', () => {
+  // on cache la popup
+  confirmWindow.classList.remove("active-confirm-window")
+})
--- templates/user/infos.php
+++ templates/user/infos.php
@@ -1,64 +1,80 @@
 <form action="<?= $__path__ ?>config/process/edit-profile-process.php" method="POST">
 
-<section class="profile-content-section">
-  <h2><i class="fi fi-rr-pencil"></i>Mes informations</h2>
+  <section class="profile-content-section">
+    <h2><i class="fi fi-rr-pencil"></i>Mes informations</h2>
 
-  <div class="content">
-    <div class="form-group">
-      <label for="prenom">Prénom</label>
-      <input type="text" value="<?= $_SESSION['user'] ?>" id="prenom" name="prenom" required>
+    <div class="content">
+      <div class="form-group">
+        <label for="prenom">Prénom</label>
+        <input type="text" value="<?= $_SESSION['user'] ?>" id="prenom" name="prenom" required>
+      </div>
+
+      <div class="form-group">
+        <label for="nom">Nom</label>
+        <input type="text" value="<?= $_SESSION['lastName'] ?>" id="nom" name="nom" required>
+      </div>
+
+      <div class="form-group">
+        <label for="email">Email</label>
+        <input type="email" value="<?= $_SESSION['email'] ?>" id="email" name="email" required>
+      </div>
+
+      <div class="form-group">
+        <label for="tel">Tel</label>
+        <input type="tel" value="<?= $_SESSION['tel'] ?>" id="tel" name="tel" required>
+      </div>
     </div>
 
-    <div class="form-group">
-      <label for="nom">Nom</label>
-      <input type="text" value="<?= $_SESSION['lastName'] ?>" id="nom" name="nom" required>
-    </div>
-    
-    <div class="form-group">
-      <label for="email">Email</label>
-      <input type="email" value="<?= $_SESSION['email'] ?>" id="email" name="email" required>
-    </div>
-    
-    <div class="form-group">
-      <label for="tel">Tel</label>
-      <input type="tel" value="<?= $_SESSION['tel'] ?>" id="tel" name="tel" required>
-    </div>
-  </div>
-
-  <input type="submit" value="Modifier mes informations" name="edit-account">
-</section>
-
-<section class="profile-content-section">
-  <h2><i class="fi fi-rr-settings"></i>Préférences</h2>
-  <section>
-    <h5>Notifications</h5>
+    <input type="submit" class="btn" value="Modifier mes informations" name="edit-account">
   </section>
-</section>
 
-<section class="profile-content-section">
-  <h2><i class="fi fi-rr-lock"></i>Mot de passe</h2>
+  <section class="profile-content-section">
+    <h2><i class="fi fi-rr-settings"></i>Préférences</h2>
+    <section>
+      <h5>Notifications</h5>
+    </section>
+  </section>
 
-  <div class="content">
-    <div class="form-group">
-      <label for="edited-password">Mot de passe</label>
-      <input type="password" name="edited-password" id="edited-password" placeholder="*****">
+  <section class="profile-content-section">
+    <h2><i class="fi fi-rr-lock"></i>Mot de passe</h2>
+
+    <div class="content">
+      <div class="form-group">
+        <label for="edited-password">Mot de passe</label>
+        <input type="password" name="edited-password" id="edited-password" placeholder="*****">
+      </div>
+
+      <div class="form-group">
+        <label for="edited-password-confirm">Confirmer le mot de passe</label>
+        <input type="password" name="edited-password-confirm" id="edited-password-confirm" placeholder="*****">
+      </div>
     </div>
 
-    <div class="form-group">
-      <label for="edited-password-confirm">Confirmer le mot de passe</label>
-      <input type="password" name="edited-password-confirm" id="edited-password-confirm" placeholder="*****">
+    <input type="submit" value="Modifier mon mot de passe" name="edit-password" class="btn">
+  </section>
+
+  <section class="profile-content-section danger-zone">
+    <h2><i class="fi fi-rr-shield-exclamation"></i>Danger zone</h2>
+    <p>Vous n'êtes pas satisfaits du contenu du site ? Vous pouvez supprimer votre compte</p>
+    <p><b>Attention cette opération est irréversible</b></p>
+
+    <button type="button" class="btn btn-danger" name="delete-account" id="delete-acc-btn">Supprimer mon compte</button>
+  </section>
+
+  <div id="del-acc-confirm-window" class="danger-zone">
+    <div class="content">
+      <h2>Voulez-vous vraiment supprimer votre compte ?</h2>
+      <p>Si oui, confirmez en tapant <b id="delete-acc-email-to-confirm"><?= $_SESSION['email'] ?></b> dans le champ çi-dessous</p>
+      <div class="form-group">
+        <label for="delete-acc-email-confirm">Confirmer l'email</label>
+        <input type="email" id="delete-acc-email-confirm">
+        <div class="btn-container">
+          <button type="button" id="cancel-deleting-acc">Annuler</button>
+          <input type="submit" value="Confirmer la suppression" name="delete-account" id="delete-acc-btn-confirm" class="btn btn-danger" disabled>
+        </div>
+      </div>
+
     </div>
   </div>
-
-  <input type="submit" value="Modifier mon mot de passe" name="edit-password">
-</section>
-
-<section class="profile-content-section danger-zone">
-  <h2><i class="fi fi-rr-shield-exclamation"></i>Danger zone</h2>
-  <p>Vous n'êtes pas satisfaits du contenu du site ? Vous pouvez supprimer votre compte</p>
-  <p><b>Attention cette opération est irréversible</b></p>
-
-  <input type="submit" value="Supprimer mon compte" name="delete-account">
-</section>
 
 </form>
